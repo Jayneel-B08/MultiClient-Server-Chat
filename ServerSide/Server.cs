f26486acab8bdb1fc1ba0796c65f7012c98faaa6
Jayneel Bhupendra <Jayneel-B08@outlook.com>
2025-12-26 14:04:01 +0000

    includes disconnection Handling and some minor exception

--- ServerSide/Server.cs
+++ ServerSide/Server.cs
@@ -8,9 +8,20 @@ namespace ServerSide
         static void Main(string[] args)
         {
             Socket ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            Connection ServerConnection = new Connection(ServerSocket, IPAddress.Loopback, 8080);
-            ServerConnection.Start();
-            Console.ReadLine();
+                Connection ServerConnection = new(ServerSocket, IPAddress.Loopback, 8080);
+                new Thread(() => ServerConnection.Start()).Start(); 
+            }
+            finally
+            {
+                Connection.ShutDown();
+            }
+            
+        }
+
+        private static void CurrentDomain_ProcessExit(object? sender, EventArgs e)
+        {
+            Connection.ShutDown();
+            //throw new NotImplementedException();
         }
     }
 }
